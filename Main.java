9aba35a826631b5b29633d46da3e25386cecf032
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-05 16:02:13 +0300

    Implement Main class with Client and Admin instances

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Client Client1 = new Client("Eyob", "Eyoba", "124578", "112546");
+                Client1.showinformation();
+        Admin Admin1= new Admin("boss","boss1","1445");
+        Admin1.showinformation();
+    }
+}
