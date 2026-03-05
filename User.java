6150f2ef8865956a35b7457fc42f8b5f7456ebf5
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-05 15:55:25 +0300

    Create User.java

---
+++ User.java
@@ -0,0 +1,15 @@
+public class User {
+    protected   String name;
+    protected   String username;
+    protected   String password;
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+    protected void showinformation() {
+        System.out.println("Name: " + name);
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+    }
+}
