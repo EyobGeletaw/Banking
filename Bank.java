8b3c54bad2653a7dd64f509d4b5ed51dce6c6e29
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-05 16:01:22 +0300

    Create Bank.java

---
+++ Bank.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private List<Client> clients = new ArrayList<>();
+    public void addClient(Client client){
+        clients.add(client);
+    }
+    public void removeClient(Client client){
+        clients.remove(client);
+    }
+    public Client login(String username, String password) {
+        for (Client client : clients) {
+            if (client.username.equals(username) && client.password.equals(password)) {
+                return client;
+            }
+        }
+        return null;
+    }
+}
