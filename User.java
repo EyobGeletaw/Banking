73ed4c0562deeef19482eb831ede2c76a38d4e50
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-06 17:18:30 +0300

    Add login method to User class

--- User.java
+++ User.java
@@ -15,4 +15,7 @@ public class User {
     public String getUsername(){
         return username;
     }
+     public boolean login( String username,String password){
+        return  this.username.equals(username) && this.password.equals(password);
+    }
 }
