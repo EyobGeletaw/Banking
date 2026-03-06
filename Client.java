627e342b5cb8003037ccc50e49162e9b0ade2ddb
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-06 17:01:51 +0300

    Refactor Client class and add user menu
    Refactored Client class to use Account object for account management and added menu for user interactions.

--- Client.java
+++ Client.java
@@ -1,11 +1,45 @@
+import java.util.Scanner;
+
 public class Client extends User {
-    String Accountnumber;
-    public Client(String Name, String Username , String Password,String Accountnumber) {
+    private Account account;
+    public Client(String Name, String Username , String Password,String AccountNumber) {
         super(Name,Username,Password);
-        this.Accountnumber=Accountnumber;
+        this.account = new Account(AccountNumber);
     }
     public void showinformation() {
         super.showinformation();
-        System.out.println("Account Number: "+Accountnumber);
+        System.out.println("Account Number: "+ account );
+
+    }
+    public void Showmenu() {
+        Scanner input = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("1. To Deposit");
+            System.out.println("2.To Withdraw");
+            System.out.println("3. To Check Balance");
+            System.out.println("4. Exit");
+            System.out.println("Enter your choice");
+            choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter Amount Number");
+                    double depositamount = input.nextDouble();
+                    account.Deposit(depositamount);
+                    break;
+                case 2:
+                    System.out.println("Enter Amount Number");
+                    double withdrawamount = input.nextDouble();
+                    account.Withdraw(withdrawamount);
+                    break;
+                case 3:
+                    System.out.println("Your Balance is" + account.getBalance());
+                    break;
+                case 4:
+                    System.out.println("Good Bye!");
+                default:
+                    System.out.println("Invalid Input");
+            }
+        }while (choice!=4);
     }
 }
