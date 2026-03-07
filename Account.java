1e6774e0f242cfc428435782fd2a08093e32fa1d
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-07 22:10:46 +0300

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,6 +1,6 @@
 public class Account {
     private String AccountNumber;
-    private Double Balance;
+    private Double alance;
     public Account(String AccountNumber)
         {
         this.AccountNumber=AccountNumber;
