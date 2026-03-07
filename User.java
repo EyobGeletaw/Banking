bde55793359d0ca2deb1bb54146645f3e0d262ae
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-07 22:19:23 +0300

    Fix syntax error in User.java

--- User.java
+++ User.java
@@ -1,5 +1,5 @@
 public class User {
-    protected   String name;
+    protected   String name
     protected   String username;
     protected   String password;
     public User(String name, String username, String password) {
