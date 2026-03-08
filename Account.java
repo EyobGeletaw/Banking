62825acaab360a4a3c362754b691645483c73507
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-08 23:08:06 +0300

    Refactor Account class for better readability
    Refactor Account class for better readability

--- Account.java
+++ Account.java
@@ -1,33 +1,33 @@
 public class Account {
-    private String AccountNumber;
-    private Double Balance;
-    public Account(String AccountNumber)
-        {
+    public String AccountNumber;
+    public Double Balance;
+    public Account(String AccountNumber) {
         this.AccountNumber=AccountNumber;
         this.Balance=0.0;
-        }
-        public String getAccountNumber(){
-            return AccountNumber;
-    }   public double getBalance(){
-        return Balance;
-    }   public void deposit(double amount) {
+    }
+    public String getAccountNumber(){
+        return AccountNumber;
+    }
+    public double getBalance(){
+            return Balance;
+    }
+    public void Deposit(double amount) {
         if (amount > 0) {
             Balance = Balance + amount;
             System.out.println("Deposited " + amount);
             System.out.println("Your currnet balance is " + Balance);
-        } else {
+        }
+        else {
             System.out.println("Invalid amount");
         }
     }
-        public void Withdraw(double amount) {
-            if (amount > 0 && Balance >= amount) {
-                Balance = Balance - amount;
-                System.out.println("Withdrawn " + amount);
-                System.out.println("your current balance is"+ Balance );
-            } else {
-                System.out.println("Ivalid amount or insufficient balance");
-            }
-
-
+    public void Withdraw(double amount) {
+        if (amount > 0 && Balance >= amount) {
+            Balance = Balance - amount;
+            System.out.println("Withdrawn " + amount);
         }
+        else {
+            System.out.println("Invalid amount or insufficient balance");
+        }
+    }
 }
