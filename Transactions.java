b5330a1c416ac67f394434f578d287a4c15dbca3
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-12 17:28:04 +0300

    Update Transactions.java

--- Transactions.java
+++ Transactions.java
@@ -12,5 +12,6 @@ private double amount;
   public void printTransactions(){
     System.out.println(type+" | From "+fromUsername+" |To "+toUsername+" |Amount "+amount);
 }
+  system.out.println("sieufkj")
 }
 
