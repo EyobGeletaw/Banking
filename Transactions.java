94e74b8e074498a7b6559b84f14169559a49892c
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-12 17:23:54 +0300

    Update Transactions.java
    implementation of the constructor class for transactions method

--- Transactions.java
+++ Transactions.java
@@ -3,3 +3,9 @@ private String type;
 private String fromUsername;
 private String toUsername;
 private double amount;
+  public void Transactions(String type,String fromUsername,String toUsername,double amount){
+    this.type=type;
+    this.fromUsername=fromUsername;
+    this.toUsername=toUsername;
+    this.amount=amount;
+}
