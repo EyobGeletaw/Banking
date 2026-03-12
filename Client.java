7dfbbc816aa97dd34a5612c36bccedec7daee102
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-12 17:31:03 +0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -7,7 +7,6 @@ public class Client extends User {
         super(Name,Username,Password);
         this.account = new Account(AccountNumber);
     }
-    @Override
     public void showInformation() {
         super.showInformation();
         System.out.println("Account Number: "+ account );
