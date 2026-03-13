9997991726019160a0bc4544c222cb8f868da77f
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-13 21:16:41 +0300

    Update Transactions.java

--- Transactions.java
+++ Transactions.java
@@ -3,14 +3,20 @@ private String type;
 private String fromUsername;
 private String toUsername;
 private double amount;
-  public void Transactions(String type,String fromUsername,String toUsername,double amount){
+public Transactions(String type, String fromUsername, String toUsername, double amount){
     this.type=type;
     this.fromUsername=fromUsername;
     this.toUsername=toUsername;
     this.amount=amount;
 }
-  public void printTransactions(){
+public void printTransactions(){
     System.out.println(type+" | From "+fromUsername+" |To "+toUsername+" |Amount "+amount);
+}
+public String getFromUsername(){
+    return fromUsername;
+}
+public String getToUsername(){
+    return toUsername;
 }
 }
 
