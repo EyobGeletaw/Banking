39a8f2cd3967d152a0906fd1e4fdd1d241a4042b
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-13 22:25:17 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,6 +7,17 @@ public class Main {
         bank.addAdmin(admin);
         do
         {System.out.println("Welcome to the Bank Application");
+         lcome to the Bank Application");
+        System.out.println("Are you Admin or Client?");
+        System.out.println("1.Admin");
+        System.out.println("2.Client");
+        Scanner input = new Scanner(System.in);
+        String answer = input.nextLine();
+        Client Loggedin =bank.login(username,password);
+                     Loggedin.showMenu();
+        }
+c static void main(String[] args) {
+        Bank bank = new Bank();
         System.out.println("Are you Admin or Client?");
         System.out.println("1.Admin");
         System.out.println("2.Client");
