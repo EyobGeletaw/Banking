775349a5511f9e7a9fc4ed7063624ea07f8d7fc1
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-13 22:23:30 +0300

    Update Main.java
    updating the menu

--- Main.java
+++ Main.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Bank bank = new Bank();
-        Client client1=new Client("Eyob","eyoba","123qw" , "1452");
-        client client2=new Client("mann","njh","kjuy","2346435")
-        bank.addClient(client1);
+       Bank bank = new Bank();
+        Admin admin=new Admin("Eyob","Eyob","12345",bank);
+        bank.addAdmin(admin);
+        do
+        {System.out.println("Welcome to the Bank Application");
+        System.out.println("Are you Admin or Client?");
+        System.out.println("1.Admin");
+        System.out.println("2.Client");
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to Bank Account");
-        System.out.println("Please enter your username");
-        String username = input.nextLine();
-        System.out.println("Please enter your password");
-        String password = input.nextLine();
+        String answer = input.nextLine();
         Client Loggedin =bank.login(username,password);
         if (Loggedin!=null){
             System.out.println("Welcome you logged in successfully " + Loggedin.getUsername());
