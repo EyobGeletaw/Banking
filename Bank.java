7c24426555c5e754a153fa28947587ace5cddfaf
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-17 22:51:12 +0300

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,27 +1,39 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Bank {
-    private ArrayList<Client> clients = new ArrayList<>();
-
-    public void addClient(Client client) {
-        clients.add(client);
+    private HashMap<String,Client> clients = new HashMap<>();
+    private ArrayList<Admin> admins = new ArrayList<>();
+    public void addClient(String username,Client client) {
+        clients.put(username,client);
     }
 
-    public void removeClient(Client client) {
-        clients.remove(client);
+    public void removeClient(String username) {
+        clients.remove(username);
+    }
+    public void addAdmin(Admin admin) {
+        admins.add(admin);
     }
 
     public Client login(String username, String password) {
-        for (Client client : clients) {
+        for (Client client : clients.values()) {
             if (client.username.equals(username) && client.password.equals(password)) {
                 return client;
             }
         }
         return null;
     }
+    public Admin loginAdmin(String username, String password) {
+        for (Admin admin : admins) {
+            if (admin.username.equals(username) && admin.password.equals(password)) {
+                return admin;
+            }
+        }
+        return null;
+    }
 
     public Client findClients(String username) {
-        for (Client client : clients) {
+        for (Client client : clients.values()) {
             if (client.username.equals(username)) {
                 return client;
             }
@@ -29,7 +41,7 @@ public class Bank {
         return null;
     }
 
-    public ArrayList<Client> getClients() {
+    public HashMap<String, Client> getClients() {
         return clients;
     }
 
@@ -52,7 +64,7 @@ public class Bank {
             transactions.add(t);
         }
         else{
-            System.out.println("Transfer failed insufficient balance") ;
+            System.out.println("Transfer failed insufficient balance");
     }
 }
 public ArrayList<Transactions> transactions = new  ArrayList<>();
@@ -65,6 +77,7 @@ public void deposit(String username, double amount) {
             client.getAccount().Deposit(amount);
             Transactions t = new Transactions("Deposit","Cash",username,amount);
             transactions.add(t);
+            totalBankBalance+=amount;
 }
 }
 public void withdraw(String username, double amount) {
@@ -73,6 +86,7 @@ public void withdraw(String username, double amount) {
             client.getAccount().Withdraw(amount);
             Transactions t = new Transactions("Withdraw",username,"Cash",amount);
             transactions.add(t);
+            totalBankBalance-=amount;
         }
 }
 public void showClientsTransaction(String username) {
@@ -81,5 +95,40 @@ public void showClientsTransaction(String username) {
             t.printTransactions();
         }
         }
+}
+private Integer numberOfClientTransaction;
+public Integer numberOfTransactions(String username) {
+        for(Transactions t : transactions){
+            if (t.getFromUsername().equals(username)||t.getToUsername().equals(username)) {
+                numberOfClientTransaction++;
+
+            }
+        }
+        return numberOfClientTransaction;
+}
+public Double totalBankBalance;
+public Double bankBalance(){
+    return totalBankBalance;
+}
+public Double totalAvailableLoan=totalBankBalance/2;
+public  Double bankAvailableLoan(){
+    return totalAvailableLoan;
+
+}
+public ArrayList<Loan> listOfLoans =new ArrayList<>();
+public void addLoan(Loan loan) {
+    listOfLoans.add(loan);
+}
+public ArrayList<Loan> getLoans() {
+    return listOfLoans;
+}
+public ArrayList<Loan> activeLoans =new ArrayList<>();
+public ArrayList<Loan> getActiveLoans() {
+    for(Loan loan : listOfLoans){
+        if (loan.getStatus()=="Accepted"){
+            activeLoans.add(loan);
+        }
+    }
+    return activeLoans;
 }
 }
