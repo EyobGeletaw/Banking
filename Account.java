94b70148133fed2e891e9b9723585ffef555e4a9
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-24 21:26:20 +0300

    Update Account.java

--- Account.java
+++ Account.java
@@ -7,9 +7,18 @@ public class Account {
     }
 }
 
-
-
-    
+public void Deposit(double amount) {
+        if (amount > 0) {
+            Balance = Balance + amount;
+            System.out.println("your account has been credited " + amount);
+            System.out.println("Your current balance is " + Balance);
+        }
+        else {
+            System.out.println("Invalid amount");
+        
+      }
+}
+ 
     public void Withdraw(double amount) {
         if (amount > 0 && Balance >= amount) {
             Balance = Balance - amount;
