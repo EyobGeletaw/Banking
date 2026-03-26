357be43c9d4a3c5a920a7c1702ee429d93419a7a
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-26 16:00:40 +0300

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,7 +1,6 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Admin extends User {
-    private Bank bank;
 
     public Admin(String Name, String Username , String Password, Bank bank)
     {
