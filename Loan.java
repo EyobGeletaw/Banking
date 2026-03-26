4acbae9739c1646bd478fe51fb3f5f5a38a0f20b
Eyob Geletaw <geletaweyob@gmail.com>
2026-03-26 16:04:38 +0300

    Update Loan.java

--- Loan.java
+++ Loan.java
@@ -3,12 +3,22 @@ import java.time.LocalDateTime;
 public class Loan {
     String username;
     String status;
-    public Loan(String username,double amount,String status ){
+    double amount;
+    public Loan(String username,double amount ){
+        this.amount=amount;
         this.username=username;
-        this.status=status;
+        this.status="PENDING";
     }
 public String getStatus(){
         return status;
 }
+public String getUsername(){
+        return username;
 }
-
+public double getAmount(){
+        return this.amount;
+}
+public void setStatus(String status){
+        this.status=status;
+}
+}
